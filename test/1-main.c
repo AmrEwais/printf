c39da333ee352d5fa20993334fd5fcca11783aac
AmrEwais <amrewais52@gmail.com>
2023-04-26 21:24:27 +0000

    Done by Amr ✅

---
+++ test/1-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%S\n", "Holberton\nSchool");
+    return (0);
+}
+
---
+++ test/holberton-main.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("Unknown:[%r]\n", "hello");
+    /*printf("Unknown:[%r]\n");*/
+    return (0);
+}
+
